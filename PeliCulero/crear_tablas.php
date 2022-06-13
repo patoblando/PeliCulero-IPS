a379285e23f5870ac6966f4d5322f37a1bff1912
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-06-12 22:01:36 -0300

    faltaba la columna email

--- PeliCulero/crear_tablas.php
+++ PeliCulero/crear_tablas.php
@@ -1,12 +1,13 @@
 <?php 
 include "variables.inc";
 $mysqli = new mysqli($host, $user, $pass, $base);
-
 $mysqli->query("drop table if exists users");
-$registro = $mysqli->query("create table if not exists users (
-	username	varchar(60),
-	pass		varchar(60),
-	id		int not null primary key auto_increment
-)");
 
+
+$registro = $mysqli->query("create table if not exists users (
+	id		int not null primary key auto_increment,
+	username	varchar(60),
+	email		varchar(60),
+	pass		varchar(60)
+)");
 ?>
