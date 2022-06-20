01d1fa9c524b1c8fb0554bf7e0ecac11ab7055f5
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-06-19 23:29:35 -0300

    el panel de admin permite banear y desbanear

--- PeliCulero/admin/ajax.js
+++ PeliCulero/admin/ajax.js
@@ -1,4 +1,4 @@
-// let response = []
+// Trae la información de la base de datos y la muestra en el DOM
 const getData = async () => {
     var requestOptions = {
         method: 'GET',
@@ -8,37 +8,64 @@ const getData = async () => {
         let response = await fetch("./API/info.php", requestOptions)
         response = await response.text()
         response = JSON.parse(response)
-        // console.log(response)
         renderData(response)
-        // return response
     } catch (e) {
         console.log(e);
-        // return []
     }
-
 }
-
 
 let table = document.getElementById('table')
 
-
+// Renderiza cada fila
 const renderData = (data) => {
     console.log(data)
     for(elem of data){
-        let id = document.createElement('div')
-        let user = document.createElement('div')
-        let active = document.createElement('div')
-        let ban = document.createElement('div')
-        id.innerHTML = JSON.stringify(elem.id)
-        user.innerHTML = JSON.stringify(elem.username)
-        active.innerHTML = JSON.stringify(elem.active)
-        ban.innerHTML = JSON.stringify(elem.ban)
-        table.appendChild(id);
-        table.appendChild(user);
-        table.appendChild(active);
-        table.appendChild(ban);
+        renderElement(elem.id)
+        renderElement(elem.username)
+        renderElement(elem.active)
+        renderElement(elem.ban)
+        renderButtonBan(elem)
     }
 }
 
+// Renderiza cada elemento
+const renderElement = (elem) => {
+    let div = document.createElement('div')
+    div.innerHTML = JSON.stringify(elem).replace(/['"]+/g, '')
+    table.appendChild(div)
+}
+
+const renderButtonBan = x => {
+    if(x.ban == 0 || x.ban == 1){
+        let div = document.createElement('div')
+        let button = document.createElement('button')
+        button.setAttribute("user-id", x.id);
+        button.addEventListener( 'click', e => {
+            let id = e.target.getAttribute('user-id')
+            toggleBan(id)
+        })
+        button.innerHTML = x.ban == 1 ? 'Desbanear' : 'Banear'
+        div.appendChild(button)
+        table.appendChild(div)
+    }
+}
+
+// Si el usuario tiene ban, se lo quita y si no lo tiene, se lo pone
+const toggleBan = (id) => {
+    var formdata = new FormData();
+    formdata.append("id", id);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("./API/toggleBan.php", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .then(location.reload())
+      .catch(error => console.log('error', error));
+}
 
 getData()
--- PeliCulero/admin/index.php
+++ PeliCulero/admin/index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Peliculero</title>
-	<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin - Peliculero</title>
+    	<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
     
-    <section id="table">
-            
-    </section>
+        <main>
+            <section id="table">
+                <div class="titulos">ID</div>    
+                <div class="titulos">Username</div>    
+                <div class="titulos">Activo</div>    
+                <div class="titulos">Ban</div>    
+                <div class="titulos"></div>    
+            </section>
+        </main>
         
     </body>
     <script src="ajax.js"></script>
